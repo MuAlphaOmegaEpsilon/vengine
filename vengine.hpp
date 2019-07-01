78b8a655b79c21e566e4965a6c4c15c75404d703
Tommaso Bonvicini <tommasobonvicini@gmail.com>
2019-07-01 19:59:51 +0200

    Create first empty version of vengine.hpp

---
+++ vengine.hpp
@@ -0,0 +1,18 @@
+/**
+ * @brief A C++17 Vulkan barebone engine. 
+ * @file vengine.hpp
+ * @author Tommaso Bonvicini <tommasobonvicini@gmail.com> https://github.com/MuAlphaOmegaEpsilon/vengine
+ * @date 01-07-2019
+ */
+
+#include <vulkan/vulkan.h>
+
+#ifndef VENGINE_HPP
+#define VENGINE_HPP
+
+namespace vengine
+{
+
+}
+
+#endif
