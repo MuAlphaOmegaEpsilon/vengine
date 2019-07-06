e088e1da4e5224dced38c45401d85ae2079875aa
MuAlphaOmegaEpsilon <tommasobonvicini@gmail.com>
2019-07-06 11:19:02 +0200

    Update script installVulkan.sh

--- scripts/installVulkan.sh
+++ scripts/installVulkan.sh
@@ -11,23 +11,25 @@ NOCOLOR=$(tput sgr0)
 ### SCRIPT CORE ###
 printf "%sINSTALLING VULKAN-SDK%s\\n" "${ORANGE}" "${NOCOLOR}"
 VK_VERSION=1.1.108.0
-LUNARG_URL=https://sdk.lunarg.com/sdk/download/${"VK_VERSION"}
+LUNARG_URL=https://sdk.lunarg.com/sdk/download/"${VK_VERSION}"
 
 case "$(uname -s)" in
-   Darwin) # osx
-    wget ${"LUNARG_URL"}/macos/vulkansdk-macos-${"VK_VERSION"}.tar.gz 
-    tar xf vulkansdk-macos-${"VK_VERSION"}.tar.gz
-    mv ${"VK_VERSION"}/ vulkan-sdk/
-    .vulkan-sdk/install_vulkan.py
-    ;;
-   Linux) # linux
-    wget ${"LUNARG_URL"}/linux/vulkansdk-linux-${"VK_VERSION"}.tar.gz 
-    tar xf vulkansdk-linux-${"VK_VERSION"}.tar.gz
-    mv ${"VK_VERSION"}/ vulkan-sdk/
-    .vulkan-sdk/setup-env.sh
-    ;;
-   CYGWIN*|MINGW32*|MSYS*) # windows
-    wget ${"LUNARG_URL"}/windows/VulkanSDK-${"VK_VERSION"}-Installer.exe
-    VulkanSDK-${"VK_VERSION"}-Installer.exe /S
-    ;;
+    Darwin) # osx
+        wget "${LUNARG_URL}"/macos/vulkansdk-macos-"${VK_VERSION}".tar.gz 
+        tar xf vulkansdk-macos-"${VK_VERSION}".tar.gz
+        mv "${VK_VERSION}"/ vulkan-sdk/
+        .vulkan-sdk/install_vulkan.py
+        ;;
+    Linux) # linux
+        wget "${LUNARG_URL}"/linux/vulkansdk-linux-"${VK_VERSION}".tar.gz 
+        tar xf vulkansdk-linux-"${VK_VERSION}".tar.gz
+        mv "${VK_VERSION}"/ vulkan-sdk/
+        .vulkan-sdk/setup-env.sh
+        ;;
+    CYGWIN*|MINGW32*|MSYS*) # windows
+        wget "${LUNARG_URL}"/windows/VulkanSDK-"${VK_VERSION}"-Installer.exe
+        VulkanSDK-"${VK_VERSION}"-Installer.exe /S
+        ;;
+    *)
+        ;;
 esac
