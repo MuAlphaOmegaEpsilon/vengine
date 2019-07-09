94d675db6805e2b14c0b98e3b9f1317ca182c7fb
MuAlphaOmegaEpsilon <tommasobonvicini@gmail.com>
2019-07-09 20:02:35 +0200

    Comment unneeded vars

--- scripts/exportVulkanVariables.sh
+++ scripts/exportVulkanVariables.sh
@@ -13,11 +13,12 @@ case "$(uname -s)" in
         MOLTENVK_DIR="$(pwd)/vulkansdk-macos-${VULKAN_VERSION}/MoltenVK" && export MOLTENVK_DIR
         ;;
     Linux) # linux
-        VULKAN_SDK="$(pwd)/vulkan-sdk/x86_64" && export VULKAN_SDK
-        Vulkan_LIBRARY="${VULKAN_SDK}/lib" && export Vulkan_LIBRARY
-        PATH="${VULKAN_SDK}/bin:$PATH" && export PATH
-        LD_LIBRARY_PATH="${VULKAN_SDK}/lib:${LD_LIBRARY_PATH:-}" && export LD_LIBRARY_PATH
-        VK_LAYER_PATH="${VULKAN_SDK}/etc/vulkan/explicit_layer.d" && export VK_LAYER_PATH
+        Vulkan_LIBRARY="/usr/lib/x86_64-linux-gnu" && export Vulkan_LIBRARY
+        # VULKAN_SDK="$(pwd)/vulkan-sdk/x86_64" && export VULKAN_SDK
+        # Vulkan_LIBRARY="${VULKAN_SDK}/lib" && export Vulkan_LIBRARY
+        # PATH="${VULKAN_SDK}/bin:$PATH" && export PATH
+        # LD_LIBRARY_PATH="${VULKAN_SDK}/lib:${LD_LIBRARY_PATH:-}" && export LD_LIBRARY_PATH
+        # VK_LAYER_PATH="${VULKAN_SDK}/etc/vulkan/explicit_layer.d" && export VK_LAYER_PATH
         ;;
     CYGWIN*|MINGW32*|MSYS*) # windows
         VULKAN_SDK="C:\\VulkanSDK\\${VULKAN_VERSION}" && export VULKAN_SDK
