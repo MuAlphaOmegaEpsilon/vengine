fb0d22e03e2b07c43dfe488e9770272ca33ee483
Tommaso Bonvicini <tommasobonvicini@gmail.com>
2019-07-28 18:39:18 +0200

    Add vulkan support for Linux distros (#1)

--- tests/test_initializeVulkan.cpp
+++ tests/test_initializeVulkan.cpp
@@ -2,8 +2,9 @@
 
 int main ()
 {
-    if (vengine::initializeVulkan("TEST APP", VK_MAKE_VERSION(1, 0, 0)))
-        return 1;
+    const VkError error = vengine::initializeVulkan("TEST APP", VK_MAKE_VERSION(1, 0, 0));
+    if (error)
+        return error;
     vengine::destroyVulkan();
     return 0;
 }
