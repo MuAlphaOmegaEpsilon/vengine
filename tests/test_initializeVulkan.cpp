6ea249e667ea1defc9f265a60a6707c8c06fe1b3
Tommaso Bonvicini <tommasobonvicini@gmail.com>
2019-08-16 19:33:51 +0200

    Simplify test case by using new shiny MACRO (#23)

--- tests/test_initializeVulkan.cpp
+++ tests/test_initializeVulkan.cpp
@@ -2,9 +2,9 @@
 
 int main ()
 {
-    const VkError error = vengine::vulkan::initialize("TEST APP", VK_MAKE_VERSION(1, 0, 0));
-    if (error)
-        return error;
+    // The RET_ERR macro avoids to manually write some boilerplate to check if
+    // an error occurred and to eventually return it.
+    RET_ERR (vengine::vulkan::initialize("myApp", VK_MAKE_VERSION(1, 0, 0)));
     vengine::vulkan::destroy();
     return 0;
 }
